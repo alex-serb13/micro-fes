4b428683be3d958e1a8c2cd5d8d5103c6de310ed
alex-serb13 <alex.serb@globant.com>
2023-01-02 20:06:47 +0200

    remove some of the shared dependencies

--- main/config/webpack.prod.js
+++ main/config/webpack.prod.js
@@ -5,6 +5,10 @@ const { dependencies } = require("../package.json");
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
+// Work-around for Uncaught (in promise) TypeError: _emotion_styled__WEBPACK_IMPORTED_MODULE_0__ is not a function
+delete dependencies["@emotion/styled"];
+delete dependencies["swr"];
+
 const prodConfig = {
   mode: "production",
   output: {
--- store/config/webpack.prod.js
+++ store/config/webpack.prod.js
@@ -3,7 +3,9 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const commonConfig = require("./webpack.common");
 const { dependencies } = require("../package.json");
 
-const domain = process.env.PRODUCTION_DOMAIN;
+// Work-around for Uncaught (in promise) TypeError: _emotion_styled__WEBPACK_IMPORTED_MODULE_0__ is not a function
+delete dependencies["@emotion/styled"];
+delete dependencies["swr"];
 
 const prodConfig = {
   mode: "production",
